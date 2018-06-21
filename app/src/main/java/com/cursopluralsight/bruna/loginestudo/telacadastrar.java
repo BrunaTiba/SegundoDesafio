f24635306baf2176865254aee2b9abf26bbf0658
BrunaTiba <brunaspinolatiba@gmail.com>
2018-06-21 13:47:44 -0300

    Adicionando o layout da tela cadastro

--- app/src/main/java/com/cursopluralsight/bruna/loginestudo/telacadastrar.java
+++ app/src/main/java/com/cursopluralsight/bruna/loginestudo/telacadastrar.java
@@ -8,23 +8,56 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class telacadastrar extends AppCompatActivity {
-    private EditText nome, senha;
-    private Button salvar;
 
-    private SharedPreferences preferences;
-    private SharedPreferences.Editor editor;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telacadastrar);
 
-        nome = findViewById(R.id.nomeCadastarEditText);
-        senha= findViewById(R.id.senhaCadastrarEditText);
-        salvar = findViewById(R.id.btSalvar);
+        final EditText nome = (EditText) findViewById(R.id.nomeCadastarEditText);
+        final EditText senha = (EditText) findViewById(R.id.senhaCadastrarEditText);
+
+        Button gravar = (Button) findViewById(R.id.btSalvar);
+        gravar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences prefs = getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+                SharedPreferences.Editor ed= prefs.edit();
+
+                ed.putString ("nome", nome.getText().toString());
+                ed.putString ("senha", senha.getText().toString());
+
+                ed.apply();
+                Toast.makeText(getBaseContext(), "Cadastro realizado com sucesso", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+        Button limpar = (Button) findViewById(R.id.btLimpar);
+        limpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nome.setText("");
+                senha.setText("");
+            }
+        });
+        Button recuperar = (Button) findViewById(R.id.btnRecuperar);
+        recuperar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences prefs = getSharedPreferences("Preferencias", Context.MODE_PRIVATE);
+                nome.setText(prefs.getString("nome", "Não tem"));
+                senha.setText(prefs.getString("email", "Não tem"));
+            }
+        });
 
 
 
